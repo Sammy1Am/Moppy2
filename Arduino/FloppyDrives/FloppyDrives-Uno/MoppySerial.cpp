756b6f3ef6c70cfc63c21eb2670c8d926e2a11ec
SammyIAm <greeneggsandsamuel@gmail.com>
2017-11-13 23:27:03 -0800

    Implemented first-pass at Arduino Uno code
    My C++ is still rather green, so there's some organizing to do for sure; the main goal was parity with the current Arduino code so we can get up and running again.  Made some changes though:
    * 9 drives supported out of the box
    * Most arrays are now indexed by drive-number rather than pin number
    * Note periods live here now
    * Changed current and MAX position to unsigned int for stepper motors with a lot of steps

---
+++ Arduino/FloppyDrives/FloppyDrives-Uno/MoppySerial.cpp
@@ -0,0 +1,92 @@
+#include "MoppySerial.h"
+
+/*
+ * Serial communications implementation for Arduino.  Handler
+ * functions are called to consume system and device messages received from
+ * the network.
+ */
+MoppySerial::MoppySerial(handleSystemMessage sys, handleDeviceMessage dev) {
+  messageBuffer[0] = START_BYTE;
+  systemHandler = sys;
+  deviceHandler = dev;
+}
+
+void MoppySerial::begin(long baud) {
+  Serial.begin(baud);
+}
+
+/* MoppyMessages contain the following bytes:
+ *  0    - START_BYTE (always 0x4d)
+ *  1    - Device address (0x00 for system-wide messages)
+ *  2    - Sub address (Only for non-system messages)
+ *  3    - Size of message body (number of bytes following this one)
+ *  4    - Command byte
+ *  5... - Optional payload
+ */
+
+void MoppySerial::readMessages() {
+
+  // If we're waiting for position 4, then we know how many bytes we're waiting for, no need
+  // to start reading until they're all there.
+  // TODO: This will break for large messages because the Arduino buffer size is only 64 bytes.
+  // This should be optimized a bit.
+  while ((messagePos != 4 && Serial.available()) 
+      || (messagePos == 4 && Serial.available() >= messageBuffer[3])) {
+        
+    switch (messagePos) {
+      case 0:
+        if (Serial.read() == START_BYTE) {
+          messagePos = 1;
+        }
+        break;
+      case 1:
+        messageBuffer[1] = Serial.read(); // Read device address
+        
+        if (messageBuffer[1] == SYSTEM_ADDRESS) {
+          messagePos = 3; // System messages have no subAddress, skip to body size
+          break;
+        }
+
+        // For Serial communications it's extremely unlikely that we'll be receiving messages not meant
+        // for us, but this can help squash noise from being treated as a message
+        if (messageBuffer[1] != DEVICE_ADDRESS) {
+          messagePos = 0; // This message isn't for us
+          break;
+        }
+        
+        messagePos = 2; // Get subAddress next
+        break;
+      case 2:
+        messageBuffer[2] = Serial.read(); // Read sub address
+
+        if (messageBuffer[2] == 0x00 || (messageBuffer[2] >= MIN_SUB_ADDRESS && messageBuffer[2] <= MAX_SUB_ADDRESS)) {
+          messagePos++; // Valid subAddress, continue
+          break;
+        }
+        
+        messagePos = 0; // Not listening to this subAddress, skip this message
+        break;
+      case 3:
+        messageBuffer[3] = Serial.read(); // Read message body size
+        messagePos++;
+        break;
+      case 4:
+        // Read command and payload
+        Serial.readBytes(messageBuffer + 4, messageBuffer[3]);
+
+        // Call appropriate handler
+        if (messageBuffer[1] == SYSTEM_ADDRESS) {
+          systemHandler(messageBuffer[4], &messageBuffer[5]);
+        } else {
+          deviceHandler(messageBuffer[2], messageBuffer[4], &messageBuffer[5]);
+        }
+
+       messagePos = 0; // Start looking for a new message
+    }
+  }
+}
+
+void MoppySerial::sendPong() {
+  Serial.write(pongBytes, 7);
+}
+
