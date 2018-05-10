28c67143677ed5585cb67832bb74434db25b9792
SammyIAm <greeneggsandsamuel@gmail.com>
2018-05-10 00:13:03 -0700

    Pong fix and javadoc rearrangement

--- Java/MoppyLib/src/main/java/com/moppy/core/comms/NetworkReceivedMessage.java
+++ Java/MoppyLib/src/main/java/com/moppy/core/comms/NetworkReceivedMessage.java
@@ -5,11 +5,11 @@ package com.moppy.core.comms;
  * adds that info for use locally.
  */
 public class NetworkReceivedMessage extends MoppyMessage {
-    
-    private final String networkType; // Type of network (e.g. BridgeUDP or BridgeSerial)
-    private final String networkIdentifier; // Unique identifier for this network (e.g. multicast IP:port or COM port name)
-    private final String remoteIdentifier; // Identifier of remote device on the network if available (e.g. IP address)
-    
+
+    private final String networkType;
+    private final String networkIdentifier;
+    private final String remoteIdentifier;
+
     public NetworkReceivedMessage(byte[] messageBytes, String networkType, String networkIdentifier, String remoteIdentifier) {
         super(messageBytes);
         this.networkType = networkType;
@@ -17,14 +17,24 @@ public class NetworkReceivedMessage extends MoppyMessage {
         this.remoteIdentifier = remoteIdentifier;
     }
 
+    /**
+     * @return Type of network (e.g. BridgeUDP or BridgeSerial)
+     */
+
     public String getNetworkType() {
         return networkType;
     }
 
+    /**
+     * @return Unique identifier for this network (e.g. multicast IP:port or COM port name)
+     */
     public String getNetworkIdentifier() {
         return networkIdentifier;
     }
 
+    /**
+     * @return Identifier of remote device on the network if available (e.g. IP address)
+     */
     public String getRemoteIdentifier() {
         return remoteIdentifier;
     }
