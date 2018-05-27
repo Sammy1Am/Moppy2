c3a7ee362a4d375b2c23a402907f8e72c0d12bbf
SammyIAm <greeneggsandsamuel@gmail.com>
2018-05-26 17:28:13 -0700

    Added ShiftRegister instrument

---
+++ Arduino/Moppy/src/MoppyInstruments/ShiftRegister.cpp
@@ -0,0 +1,162 @@
+/*
+ * ShiftRegister.cpp
+ *
+ * Output for controlling a single device that uses shift registers, one
+ * bit for each note.  E.g. Xylophones or drums
+ */
+#include "ShiftRegister.h"
+
+#include "MoppyInstrument.h"
+
+// Define pins for connection to shift registers
+#define DATA_PIN 2
+#define SHIFT_CLOCK_PIN 3
+#define LATCH_PIN 4
+#define MASTER_CLEAR_PIN 5
+#define OUTPUT_ENABLE_PIN 6
+
+// First and last supported notes (any notes outside this range will be ignored, first note will be
+// indexed as zero for shifting
+#define NUM_NOTES 24
+const uint8_t FIRST_NOTE = 48;
+const uint8_t LAST_NOTE = FIRST_NOTE + NUM_NOTES;
+
+#define SHIFT_TIMER_RESOLUTION 1000 //Microsecond resolution for starting and ending notes
+#define PULSE_TICKS 50 // Length of "on" pulse for each bit in ticks (e.g. RESOLUTION * PULSE_TICKS microseconds)
+
+#define SHIFT_DATA_BYTES 3
+uint8_t ShiftRegister::shiftData[SHIFT_DATA_BYTES]; // 8*3 = 24
+uint8_t ShiftRegister::activeTicksLeft[NUM_NOTES];
+
+boolean ShiftRegister::shouldShift = false; // When true, a shift will occur during the next tick
+
+void ShiftRegister::setup() {
+
+  // Prepare pins
+  pinMode(DATA_PIN, OUTPUT);
+  pinMode(SHIFT_CLOCK_PIN, OUTPUT);
+  pinMode(LATCH_PIN, OUTPUT);
+
+  // Don't enable these unless you plan to use them!
+  //pinMode(MASTER_CLEAR_PIN, OUTPUT);
+  //pinMode(OUTPUT_ENABLE_PIN, OUTPUT);
+
+  pinMode(13, OUTPUT); // Built-in LED for blinking
+
+
+  // With all pins setup, let's do a first run reset
+  zeroOutputs();
+  delay(500); // Wait a half second for safety
+
+  // Setup timer to handle interrupts for floppy driving
+  Timer1.initialize(SHIFT_TIMER_RESOLUTION); // Set up a timer at the resolution defined in MoppyInstrument.h
+  Timer1.attachInterrupt(tick); // Attach the tick function
+}
+
+
+//
+//// Message Handlers
+//
+
+// Handles system messages (e.g. sequence start and stop)
+void ShiftRegister::systemMessage(uint8_t command, uint8_t payload[]) {
+  switch(command) {
+      // NETBYTE_SYS_PING is handled by the network adapter directly
+    case NETBYTE_SYS_RESET: // System reset
+      zeroOutputs();
+      break;
+    case NETBYTE_SYS_START: // Sequence start
+      // Nothing to do here yet
+      break;
+    case NETBYTE_SYS_STOP: // Sequence stop
+      zeroOutputs();
+      break;
+  }
+}
+
+// Handles device-specific messages (e.g. playing notes)
+void ShiftRegister::deviceMessage(uint8_t subAddress, uint8_t command, uint8_t payload[]) {
+  switch(command) {
+    case NETBYTE_DEV_RESET: // Reset
+      zeroOutputs(); // SubAddress unimportant here since we only have one
+      break;
+    case NETBYTE_DEV_NOTEON: // Note On
+      if (payload[0] >= FIRST_NOTE && payload[0] <= LAST_NOTE) {
+        outputOn(payload[0] - FIRST_NOTE);
+        activeTicksLeft[payload[0] - FIRST_NOTE] = PULSE_TICKS;
+        shouldShift = true;
+      }
+      break;
+    case NETBYTE_DEV_NOTEOFF: // Note Off
+      // Ignore these, the solenoids are pulsed the same length for all notes
+      break;
+    case NETBYTE_DEV_BENDPITCH: //Pitch bend
+      // Ignore these, we can't bend pitch
+      break;
+  }
+}
+
+//
+//// Floppy driving functions
+//
+
+/*
+Called by the timer interrupt at the specified resolution.  Because this is called extremely often,
+it's crucial that any computations here be kept to a minimum!
+ */
+void ShiftRegister::tick()
+{
+  for (byte n = 0; n < NUM_NOTES; n++) {
+    if (activeTicksLeft[n] > 0) {
+      if (--activeTicksLeft[n] == 0) {
+        outputOff(n);
+        shouldShift = true;
+      }
+    }
+  }
+
+  if (shouldShift) {
+    shouldShift = false;
+    shiftAllData();
+  }
+}
+
+////
+// UTILITY FUNCTIONS
+////
+
+//Not used now, but good for debugging...
+void blinkLED(){
+  digitalWrite(13, HIGH); // set the LED on
+  delay(250);              // wait for a second
+  digitalWrite(13, LOW);
+}
+
+
+////
+// Shifting and bitsetting functions
+////
+
+void ShiftRegister::shiftAllData()
+{
+  digitalWrite(LATCH_PIN, LOW);
+  for (int i=SHIFT_DATA_BYTES-1;i>=0;i--){
+    shiftOut(DATA_PIN, SHIFT_CLOCK_PIN, MSBFIRST, shiftData[i]);
+  }
+  digitalWrite(LATCH_PIN, HIGH);
+}
+
+void ShiftRegister::outputOn(byte outputNum){
+  bitSet(shiftData[outputNum/8],outputNum%8);
+}
+
+void ShiftRegister::outputOff(byte outputNum){
+  bitClear(shiftData[outputNum/8],outputNum%8);
+}
+
+void ShiftRegister::zeroOutputs() {
+  for (byte i=0;i<SHIFT_DATA_BYTES;i++){
+    shiftData[i] = 0;
+  }
+  shiftAllData();
+}
---
+++ Arduino/Moppy/src/MoppyInstruments/ShiftRegister.h
@@ -0,0 +1,35 @@
+/*
+ * ShiftRegister.h
+ * For bit shifters like the 74HC595N
+ */
+
+#ifndef SRC_MOPPYINSTRUMENTS_SHIFTREGISTER_H_
+#define SRC_MOPPYINSTRUMENTS_SHIFTREGISTER_H_
+
+#include <Arduino.h>
+#include <TimerOne.h>
+#include "../../MoppyConfig.h"
+#include "../MoppyNetworks/MoppyNetwork.h"
+
+class ShiftRegister {
+public:
+  static void setup();
+  static void systemMessage(uint8_t command, uint8_t payload[]);
+  static void deviceMessage(uint8_t subAddress, uint8_t command, uint8_t payload[]);
+protected:
+  static uint8_t shiftData[];
+  static uint8_t activeTicksLeft[];
+  static boolean shouldShift;
+
+  static void tick();
+  static void blinkLED();
+  static void startupSound(byte driveNum);
+  static void shiftAllData();
+  static void outputOn(byte outputNum);
+  static void outputOff(byte outputNum);
+  static void zeroOutputs();
+};
+
+
+
+#endif /* SRC_MOPPYINSTRUMENTS_SHIFTREGISTER_H_ */
