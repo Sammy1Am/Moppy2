ec60aca5c8f60cc6a05c17512eb5472f55ea8f7d
SammyIAm <greeneggsandsamuel@gmail.com>
2018-05-27 13:02:13 -0700

    Oops, accidentally changed defaults.  Fixed now.

--- Arduino/Moppy/MoppyConfig.h
+++ Arduino/Moppy/MoppyConfig.h
@@ -16,12 +16,12 @@
 
 // Device address for this Arduino (only messages sent to this address
 // will be processed.
-#define DEVICE_ADDRESS 0x02
+#define DEVICE_ADDRESS 0x01
 
 // Minimum and maximum sub-addresses that messages will be processed for.
 // E.g. if you have 8 drives this would be 1 and 8.  If you have 16, 1 and 16.
 #define MIN_SUB_ADDRESS 1
-#define MAX_SUB_ADDRESS 1
+#define MAX_SUB_ADDRESS 8
 
 
 #endif /* SRC_MOPPYCONFIG_H_ */
